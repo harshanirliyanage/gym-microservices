5206afea6b856ff23902d60c9d0d0319198aed33
Jayani  Srimali <127177432+JayaniSrimali@users.noreply.github.com>
2026-04-05 11:17:19 +0530

    Update Trainer Service with Database and DAL architecture

--- trainer-service/service.py
+++ trainer-service/service.py
@@ -1,21 +1,23 @@
-# trainer_service/service.py
-from data_service import TrainerMockDataService
+# trainer-service/service.py
+from sqlalchemy.orm import Session
+from data_service import TrainerDataService
+from models import TrainerCreate, TrainerUpdate
 
 class TrainerService:
     def __init__(self):
-        self.data_service = TrainerMockDataService()
+        self.data_service = TrainerDataService()
 
-    def get_all(self):
-        return self.data_service.get_all_trainers()
+    def get_all(self, db: Session):
+        return self.data_service.get_all_trainers(db)
 
-    def get_by_id(self, trainer_id: int):
-        return self.data_service.get_trainer_by_id(trainer_id)
+    def get_by_id(self, db: Session, trainer_id: int):
+        return self.data_service.get_trainer_by_id(db, trainer_id)
 
-    def create(self, trainer_data):
-        return self.data_service.add_trainer(trainer_data)
+    def create(self, db: Session, trainer_data: TrainerCreate):
+        return self.data_service.add_trainer(db, trainer_data)
 
-    def update(self, trainer_id: int, trainer_data):
-        return self.data_service.update_trainer(trainer_id, trainer_data)
+    def update(self, db: Session, trainer_id: int, trainer_data: TrainerUpdate):
+        return self.data_service.update_trainer(db, trainer_id, trainer_data)
 
-    def delete(self, trainer_id: int):
-        return self.data_service.delete_trainer(trainer_id)
+    def delete(self, db: Session, trainer_id: int):
+        return self.data_service.delete_trainer(db, trainer_id)
